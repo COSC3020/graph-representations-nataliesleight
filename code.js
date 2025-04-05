91380f5d1f3385695d98c104c49da9ee1e1b9381
nataliesleight <nsleight@uwyo.edu>
2025-04-05 13:00:09 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    var vertexSize = adjMatrix.length;
+    var adjList = new Array(vertexSize);
+
+    // put empty array in each slot in array
+    for (var i = 0; i < vertexSize; i++) {
+        adjList[i] = [];
+    }
+
+    // looking through whole matrix
+    for (var i = 0; i < vertexSize; i++) {          // looking through first index (incoming vertex)
+        for (var j = 0; j < vertexSize; j++) {      // looking through secon index (receiving vertex)
+            if (adjMatrx[i][j] != 0) {              // if edge exists 
+                adjList[i].push(j);                 // add receiving vertex value to incoming vertex in list
+            }
+        }
+    }
+
+    return adjList;
 }
